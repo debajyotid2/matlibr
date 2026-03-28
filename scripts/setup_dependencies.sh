9417c26a409daa3483f974db95d79cf33ae75e8c
debajyotid2 <92257044+debajyotid2@users.noreply.github.com>
2026-03-28 00:37:55 -0400

    Add CFLAGS mentioning target architecture for build for OpenBLAS installation

--- scripts/setup_dependencies.sh
+++ scripts/setup_dependencies.sh
@@ -6,6 +6,7 @@ set -xe
 DEP_DIR="../third_party"
 OPENBLAS_URL="https://github.com/OpenMathLib/OpenBLAS/releases/download/v0.3.28/OpenBLAS-0.3.28.zip"
 OPENBLAS_DIR="OpenBLAS-0.3.28"
+CFLAGS="-O2 -march=native"
 
 if [ "$1" = "" ]; then
     NUM_THREADS=1
@@ -23,7 +24,7 @@ unzip -o "${OPENBLAS_DIR}.zip"
 rm "${OPENBLAS_DIR}.zip" 
 cd $OPENBLAS_DIR
 if [ ! -f libopenblas.a ]; then
-    make -j "$NUM_THREADS"
+    make -j "${NUM_THREADS}" CFLAGS="${CFLAGS}"
 else
     echo "libopenblas.a found! Skipping build."
 fi
